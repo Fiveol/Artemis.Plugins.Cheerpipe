87dd4cd32a37b3025357378bb971dcda143e784e
Fiveol <andyinventor@outlook.com>
2025-12-23 12:00:46 -0800

    Update PluginBootstrapper.cs

--- src/Nodes/Artemis.Plugins.Nodes.Extra/PluginBootstrapper.cs
+++ src/Nodes/Artemis.Plugins.Nodes.Extra/PluginBootstrapper.cs
@@ -1,24 +1,13 @@
 using Artemis.Core;
-using Artemis.Core.Services;
-using Artemis.Plugins.Nodes.Extra.MathNodes;
 
-namespace Artemis.Plugins.Nodes.MathExtra
+namespace Artemis.Plugins.Nodes.Extra
 {
-    public class MathExtraPluginBootstrapper : PluginBootstrapper
+    public class ExtraNodesPluginBootstrapper : PluginBootstrapper
     {
-        private readonly INodeService _nodeService;
-        public MathExtraPluginBootstrapper(INodeService nodeService)
-        {
-            _nodeService = nodeService;
-        }
-
         public override void OnPluginEnabled(Plugin plugin)
         {
-            _nodeService.RegisterNode<DivideNumericsNode>();
-            _nodeService.RegisterNode<FullLerpNode>();
-            _nodeService.RegisterNode<MultiplyNode>();
-            _nodeService.RegisterNode<PercentageOfNode>();
-            _nodeService.RegisterNode<AbsNumericNode>();
+            // No manual registration needed anymore.
+            // Nodes with [Node] attribute will be discovered automatically.
         }
     }
 }
