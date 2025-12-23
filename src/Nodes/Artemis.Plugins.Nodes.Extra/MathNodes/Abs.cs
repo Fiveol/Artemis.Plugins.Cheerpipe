19e868593d71dc3835d446f634ea9b38bf65e97c
Fiveol <andyinventor@outlook.com>
2025-12-23 12:01:13 -0800

    Update Abs.cs

--- src/Nodes/Artemis.Plugins.Nodes.Extra/MathNodes/Abs.cs
+++ src/Nodes/Artemis.Plugins.Nodes.Extra/MathNodes/Abs.cs
@@ -1,36 +1,36 @@
-﻿using Artemis.Core;
+using Artemis.Core;
 using System;
 
-namespace Artemis.Plugins.Nodes.Extra.MathNodes;
-
-[Node("Abs", "Return absolute value", "Mathematics", InputType = typeof(Numeric), OutputType = typeof(Numeric))]
-public class AbsNumericNode : Node
+namespace Artemis.Plugins.Nodes.Extra.MathNodes
 {
-    #region Properties & Fields
-
-    public InputPin<Numeric> Input { get; }
-    public OutputPin<Numeric> Output { get; }
-
-    #endregion
-
-    #region Constructors
-
-    public AbsNumericNode()
+    [Node("Abs", "Return absolute value", "Mathematics", InputType = typeof(double), OutputType = typeof(double))]
+    public class AbsNumericNode : Node
     {
-        Input = CreateInputPin<Numeric>();
-        Output = CreateOutputPin<Numeric>();
+        #region Properties & Fields
+
+        public InputPin<double> Input { get; }
+        public OutputPin<double> Output { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public AbsNumericNode()
+        {
+            Input = CreateInputPin<double>();
+            Output = CreateOutputPin<double>();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public override void Evaluate()
+        {
+            Output.Value = Math.Abs(Input.Value);
+        }
+
+        #endregion
     }
-
-    #endregion
-
-    #region Methods
-
-    /// <inheritdoc />
-    public override void Evaluate()
-    {
-
-        Output.Value = Math.Abs(Input.Value);
-    }
-
-    #endregion
 }
